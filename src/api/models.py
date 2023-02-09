d431fb1348a7149a6fab9e98d8b6c9a8501d7c7b
Dan Reynolds <dreynolds2712@gmail.com>
2023-02-10 00:22:55 +0100

    Add perk and herbs to models

--- src/api/models.py
+++ src/api/models.py
@@ -52,17 +52,30 @@ class Item(models.Model):
             "slot": self.slot
         }
 
+class Perk(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
 class Character(models.Model):
     player = models.ForeignKey(User,on_delete=models.CASCADE)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    experience = models.IntegerField(default=0)
+    retired = models.BooleanField(default=False)
     notes = models.CharField(max_length=2000, default="")
+
     gold = models.IntegerField(default=0)
     wood = models.IntegerField(default=0)
     iron = models.IntegerField(default=0)
     leather = models.IntegerField(default=0)
-    experience = models.IntegerField(default=0)
-    retired = models.BooleanField(default=False)
+
+    arrowvine = models.IntegerField(default=0)
+    axenut = models.IntegerField(default=0)
+    corpsecap = models.IntegerField(default=0)
+    flamefruit = models.IntegerField(default=0)
+    rockroot = models.IntegerField(default=0)
+    snowthistle = models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.name
@@ -85,14 +98,23 @@ class Character(models.Model):
             "character_class":self.character_class.name,
             "name": self.name,
             "retired": self.retired,
+            "experience":self.experience,
+            "level" : get_level(self.experience),
+            "hand_size":self.character_class.hand_size,
             "notes": self.notes,
+
             "gold": self.gold,
             "wood":self.wood,
             "iron":self.iron,
             "leather":self.leather,
-            "experience":self.experience,
-            "level" : get_level(self.experience),
-            "hand_size":self.character_class.hand_size,
+
+            "arrowvine": self.arrowvine,
+            "axenut": self.axenut,
+            "corpsecap": self.corpsecap,
+            "flamefruit": self.flamefruit,
+            "rockroot": self.rockroot,
+            "snowthistle": self.snowthistle,
+            
             "items":[item.payload() for item in items],
             "ability_cards":[ability_card.payload() for ability_card in ability_cards]
         }
@@ -101,7 +123,7 @@ class CharacterCard(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     ability_card = models.ForeignKey(AbilityCard, on_delete=models.CASCADE)
     equipped = models.BooleanField()
-    
+
     def __str__(self):
         return f"{self.character} : {self.ability_card}"
 
@@ -112,6 +134,11 @@ class CharacterItem(models.Model):
 
     def __str__(self):
         return f"{self.character} : {self.item}"
+
+class CharacterPerk(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    perk = models.ForeignKey(Perk, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
 
 
     
