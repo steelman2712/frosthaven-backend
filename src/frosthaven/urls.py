e092bde86c68282af0278ba7f20012c40877fad5
Dan Reynolds <dreynolds2712@gmail.com>
2023-02-10 01:16:39 +0100

    Add token veirfy link

--- src/frosthaven/urls.py
+++ src/frosthaven/urls.py
@@ -19,6 +19,7 @@ from django.urls import include,path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 
@@ -27,5 +28,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_refresh'),
     path('api/', include('api.urls')),
 ]
